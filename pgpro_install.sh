a5ea38a478df1e1b00ff8cd5b427e12a147266c3
shestopalovpro <308302@gmail.com>
2022-11-19 00:07:47 +0800

    0.2.3

--- pgpro_install.sh
+++ pgpro_install.sh
@@ -3,13 +3,18 @@ sh pgpro-repo-add.sh
 
 apt-get install postgrespro-1c-14 postgrespro-1c-14-dev -y
 
-mkdir -p /data/postgres/
-chown -R postgres. /data
-
 locale-gen ru_RU.UTF-8
 localectl set-locale LANG=ru_RU.UTF-8 LC_TIME=ru_RU.UTF-8 LC_COLLATE=ru_RU.UTF-8
+update-locale LANG=ru_RU.UTF-8
+
+systemctl stop postgrespro-1c-14
+
+mkdir -p /data/postgres/
+chown -R postgres. /data
 
 # очищаем дефолтные настройки
 rm -f /etc/default/postgrespro-1c-14
 # инициализируем PostgreSQL, создавая дефолтную пустые базы из шаблонов - указание ru-локали обязательно для дальнейшей работы 1С
 /opt/pgpro/1c-14/bin/pg-setup initdb -D /data/postgres/ --locale=ru_RU.UTF-8 
+
+systemctl start postgrespro-1c-14
